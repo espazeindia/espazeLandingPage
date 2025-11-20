f1960f39e4beaa8389a628a02b439469b5c8c094
Drishti Aggarwal <espazeindia@gmail.com>
2025-11-21 00:13:52 +0530

    minute change for responsiveness

--- app/components/sections/hero.tsx
+++ app/components/sections/hero.tsx
@@ -22,6 +22,7 @@ export default function Hero() {
         <div className="absolute inset-0 bg-purple-900/30 shadow" />
       </div>
 
+
       {/* Decorative elements */}
       {/* <div className="absolute inset-0 overflow-hidden -z-10">
         <div className="absolute top-20 right-20 w-72 h-72 bg-purple-400 rounded-full mix-blend-multiply filter blur-xl opacity-20 animate-float"></div>
@@ -29,7 +30,7 @@ export default function Hero() {
         <div className="absolute top-1/2 left-1/3 w-96 h-96 bg-purple-300 rounded-full mix-blend-multiply filter blur-xl opacity-15 animate-float" style={{ animationDelay: "4s" }}></div>
       </div> */}
 
-      <div className="max-w-6xl px-2 md:px-6 py-20  md:ml-28 md:py-32 relative z-10">
+      <div className="max-w-6xl px-2 md:px-6 py-20  sm:ml-28 md:py-32 relative z-10">
         <div className="max-w-3xl mt-12">
           {/* Trust Badge */}
           <div className="inline-flex items-center gap-2 bg-white/80 backdrop-blur-sm px-4 py-2 rounded-full shadow-sm mb-6 animate-fade-in border-2 border-purple-300 bg-linear-to-br from-purple-100 to-pink-100">
